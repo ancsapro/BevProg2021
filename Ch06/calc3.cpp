83871e54d5da697462c6b2cb67b19ede4a3769b4
ancsapro <apro.aniko@inf.unideb.hu>
2021-10-12 18:55:56 +0200

    Upload calc3

--- Ch06/calc3.cpp
+++ Ch06/calc3.cpp
@@ -1,16 +1,14 @@
 #include "std_lib_facilities.h"
-
-double expession();
-
-class Token {
+double expression();
+class Token{
 public:
 	char kind;
 	double value;
-	Token (char ch): kind(ch), value(0) {}
-	Token (char ch, double val): kind(ch), value(val) {}
+	Token(char ch): kind(ch), value(0){}
+	Token (char ch, double val): kind(ch), value(val){}	
 };
 
-Token get_token() {
+Token get_token(){
 	char ch;
 	cin >> ch;
 
@@ -21,20 +19,19 @@ Token get_token() {
 		case '-':
 		case '*':
 		case '/':
-		//case '%':
+//		case '%':
 			return Token(ch);
 		case '.':
-		case '0': case '1': case '2': case '3': 
-		case '4': case '5': case '6': case '7': 
-		case '8': case '9': {
+		case '0': case '1': case '2': case '3':
+		case '4': case '5': case '6': case '7':
+		case '8': case '9':{
 			cin.putback(ch);
 			double val = 0;
 			cin >> val;
 			return Token('8', val);
-
-		} 
-		default:
-			error("Bad Token");
+		}
+		default: 
+			error("Bad token");
 	}
 }
 
@@ -42,21 +39,20 @@ double primary(){
 	Token t = get_token();
 	switch(t.kind){
 		case '(':{
-			double d = expession();
+			double d = expression();
 			t = get_token();
-			if(t.kind != ')') { error("')' expected!"); }
+			if(t.kind != ')') error("')' missing!");
 			return d;
 		}
 		case '8':
 			return t.value;
-		default: {
-		 error("Primary expected");
-		}
+		default: 
+			error("Primary missing!");
+
 	}
 }
 
 double term(){
-
 	double left = primary();
 	Token t = get_token();
 	while(true){
@@ -72,22 +68,22 @@ double term(){
 				t = get_token();
 				break;
 			}
-//			case '%':
-//				left /= primary();
+//			 case '%':
+//				left %= primary();
 //				t = get_token();
 //				break;
-			default:
+			default: 
 				return left;
+
 		}
 	}
 }
 
-double expession(){
-
+double expression(){
 	double left = term();
 	Token t = get_token();
 	while(true){
-		switch(t.kind){
+		switch (t.kind){
 			case '+':
 				left += term();
 				t = get_token();
@@ -98,22 +94,21 @@ double expession(){
 				break;
 			default:
 				return left;
+
 		}
 	}
 }
 
-int main(void)
-	
+int main(){
 try{
 	while(cin)
-		cout << expession() << endl;
-
-
+		cout << expression() << endl;
 	return 0;
 }catch (exception& e){
 	cerr << e.what() << endl;
 	return 1;
-}catch (...){
-	cerr << "Exeption" << endl;
-	return 2; 
+}catch(...){
+	cerr << "Exception" << endl;
+	return 2;
 }
+}	
